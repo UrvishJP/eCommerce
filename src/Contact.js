d14d8f9f7124cc834f28b40d0ef87dd6446a3338
Urvish Prajapati <urvishmeghani111@gmail.com>
2023-02-14 14:46:54 +0530

    made contact page

--- src/Contact.js
+++ src/Contact.js
@@ -33,7 +33,42 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return <Wrapper>
+    <h2 className="common-heading">Contact Page</h2>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.3471958884984!2d72.64345496431419!3d23.0110208849589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e86448e490c41%3A0x6bb609507d96f390!2sSanskartirth%20Society%2C%20Arbudanagar%2C%20Odhav%2C%20Ahmedabad%2C%20Gujarat%20382415!5e0!3m2!1sen!2sin!4v1676364379945!5m2!1sen!2sin" width="100%" height="400" style={{border:0}} allowFullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+    <div className="container">
+      <div className="contact-form">
+        <form action="https://formspree.io/f/moqzerge" method="POST" className="contact-inputs">
+        <input
+              type="text"
+              placeholder="name"
+              name="name"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter your message"></textarea>
+
+            <input type="submit" value="send" />
+        </form>
+      </div>
+    </div>
+  </Wrapper>;
 };
 
 export default Contact;
