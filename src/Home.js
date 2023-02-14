7950476b09f5899d4b513f422966c2eb826f5fa3
Urvish Prajapati <urvishmeghani111@gmail.com>
2023-02-14 14:08:18 +0530

    made services and trusted components

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
-import styled from "styled-components";
+import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted";
 
 const Home = () => {
-  return <Wrapper className="test">Home</Wrapper>;
+  const data = {
+    name: "Store",
+  };
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted/>
+    </>
+  );
 };
-
-const Wrapper = styled.section`
-  height: 100vh;
-  background-color: ${({ theme }) => theme.colors.bg};
-`;
 
 export default Home;
